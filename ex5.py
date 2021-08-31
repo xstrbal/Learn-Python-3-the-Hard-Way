fc6416f25916812b8aad24eabada05b247dd24ae
Lukáš Štrba <34416717+xstrbal@users.noreply.github.com>
2021-08-31 20:54:55 +0200

    More Variables and Printing

---
+++ ex5.py
@@ -0,0 +1,25 @@
+name = 'Zed A. Shaw'
+age = 35 # not a lie
+height = 74 # inches
+weight = 180 # lbs
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+
+print(f"Let's talk about {name}.")
+print(f"He's {height} inches tall.")
+print(f"He's {weight} pounds heavy.")
+print("Actaully that's not too heavy.")
+print(f"He's got {eyes} eyes and {hair} hair.")
+print(f"His teeth are usually {teeth} depending on coffee.")
+
+# this line is tricky, try to get it right
+total = age + height + weight
+print(f"If I add {age}, {height}, and {weight} I get {total}.")
+
+
+height_cm = height * 2.54
+print(f"He's {height_cm} cm tall.")
+
+weight_kg = weight * 0.45359237
+print(f"He's {weight_kg} kg heavy.")
